b39bdff3a8881d680309108d71a367db3e317cfb
Harshil Patel <pharshil748@gmail.com>
2025-06-04 15:32:23 +0530

    code changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500">
+			<h1 className="text-4xl font-bold text-white">✅ Tailwind is working!</h1>
+		</div>
+	);
 }
 
 export default App;
