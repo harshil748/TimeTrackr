72fafeaa3fe30e48fc96d0e604edb69c174a95a7
Harshil Patel <pharshil748@gmail.com>
2025-06-19 15:36:25 +0530

    style: fix formatting for consistency in App component

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,16 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Reports from "./pages/Reports";
 import Signup from "./pages/Signup";
-import AdminDashboard from "./pages/AdminDashboard";;
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={ <Login /> } />
+				<Route path="/" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
 				<Route path="/dashboard" element={<Dashboard />} />
-				<Route path="/reports" element={ <Reports /> } />
+				<Route path="/reports" element={<Reports />} />
 				<Route path="/admin" element={<AdminDashboard />} />
 			</Routes>
 		</Router>
